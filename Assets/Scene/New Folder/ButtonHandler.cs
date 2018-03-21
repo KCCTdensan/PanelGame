f89c45d6f890f3e37da159e8a03d05d76879d25a
Shioi-taisei <oco6832@gmail.com>
2018-03-21 16:05:33 +0900

    ボタンを追加しました

---
+++ Assets/Scene/New Folder/ButtonHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backscript : MonoBehaviour {
+
+	void Start () {
+	}
+
+	void Update () {
+	}	
+    public void OnClick() { // 必ず public にする
+			Debug.Log ("clicked");
+				
+	}
+}
