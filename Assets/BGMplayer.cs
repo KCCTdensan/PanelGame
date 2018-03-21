002638a2a691d05dbd24d41b2095409928407d8f
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-03-21 14:58:15 +0900

    BGMplayer実装 loopに遅延有り

---
+++ Assets/BGMplayer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMplayer : MonoBehaviour {
+
+    [SerializeField] AudioClip IntroClip;
+    [SerializeField] AudioClip LoopClip;
+
+    AudioSource IntroSource;
+    AudioSource LoopSource;
+
+	// Use this for initialization
+	void Start () {
+        IntroSource = gameObject.AddComponent<AudioSource>();
+        LoopSource = gameObject.AddComponent<AudioSource>();
+
+        IntroSource.clip = IntroClip;
+        IntroSource.loop = false;
+        IntroSource.playOnAwake = false;
+
+        LoopSource.clip = LoopClip;
+        LoopSource.loop = true;
+        LoopSource.playOnAwake = false;
+
+        playBGM();
+    }
+
+// Update is called once per frame
+    void playBGM () {
+        if (IntroSource == null || LoopSource == null)
+        {
+            return;
+        }
+
+        IntroSource.Play();
+        LoopSource.PlayScheduled(AudioSettings.dspTime + IntroClip.length);
+    }
+
+    public void StopBGM()
+    {
+        if (IntroSource == null || LoopSource == null)
+        {
+            return;
+        }
+
+        if (IntroSource.isPlaying)
+        {
+            IntroSource.Stop();
+        }
+        else if (LoopSource.isPlaying)
+        {
+            LoopSource.Stop();
+        }
+    }
+}
