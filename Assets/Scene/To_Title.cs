9fb194e5a791ea1be00d6d8325bcf26c1163801b
SIGAKNIGHT <34705179+SIGAKNIGHT@users.noreply.github.com>
2018-03-28 15:29:16 +0900

    Titleに戻るボタン追加

---
+++ Assets/Scene/To_Title.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class To_Title : MonoBehaviour {
+
+
+    public void OnClick(){
+        SceneManager.LoadScene("Title");
+    }
+}
