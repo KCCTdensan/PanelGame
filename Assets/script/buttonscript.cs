74b4e40c1dcf6702712efdf6ad91dcfe0cca805f
Shioi-taisei <oco6832@gmail.com>
2018-05-12 19:12:05 +0900

    Timerの追加とseane移動の途中のやつです

--- Assets/script/buttonscript.cs
+++ Assets/script/buttonscript.cs
@@ -1,23 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class buttonscript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButtonDown(0)){
-            SceneManager.LoadScene("StageSelect");
-              }
-        }
+
+		if (Input.GetMouseButtonDown (0)) {
+			SceneManager.LoadScene ("StageSelect");
+		}	
+	}
 }
-
-
-
-
-
