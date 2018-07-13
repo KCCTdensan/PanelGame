30977d0de8c0328a8c7e27ae789e38c2bcea366d
kobabayashi <tmzbdgfg3ywu9t67f163@docomo.ne.jp>
2018-07-13 18:57:01 +0900

    スコアスクリプト追加

---
+++ Assets/Scripts/scoreScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI; 
+
+public class scoreScript : MonoBehaviour {
+
+	//点数を格納する変数
+	public int score = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		this.GetComponent<Text>().text = "１位" + score.ToString() + "点";
+	}
+}
