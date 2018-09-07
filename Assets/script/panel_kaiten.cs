fc0030fc33f15f94fd039034e53c41bcba1d560c
Shioi <oco6832@gmail.com>
2018-09-07 11:50:51 +0900

    パネルの回転機構作成

---
+++ Assets/script/panel_kaiten.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class panel_kaiten : MonoBehaviour {
+   
+    [SerializeField]
+    private bool turning = false;
+    [SerializeField]
+    private bool startzero = false;
+    [SerializeField]
+    private bool startmax = false;
+    private float nowtime = 0;
+    public void turn()
+    {
+        turning = true;
+    }
+	// Use this for initialization
+	void Start () {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float minAngle = 0.0F;
+        float maxAngle = 180.0F;
+        float minAngle2 = 180.0F;
+        float maxAngle2 = 0.1F;
+
+       
+        if (turning)
+        {
+            nowtime += Time.deltaTime;
+            if (transform.eulerAngles.y == 0 || startzero)
+            {
+                startzero = true;
+                float angle = Mathf.LerpAngle(minAngle, maxAngle, nowtime);
+                transform.eulerAngles = new Vector3(0, angle, 0);
+                if (transform.eulerAngles.y == 180)
+                {
+                    turning = false;
+                    startzero = false;
+                    nowtime = 0;
+                }
+            }
+            else if (transform.eulerAngles.y == 180 || startmax)
+            {
+                startmax = true;
+                float angle = Mathf.LerpAngle(minAngle2, maxAngle2, nowtime);
+                transform.eulerAngles = new Vector3(0, angle, 0);
+                Debug.Log(transform.eulerAngles.y);
+                if (transform.eulerAngles.y <= 5)
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                }
+                if (transform.eulerAngles.y == 0)
+                {
+                    turning = false;
+                    startmax = false;
+                    nowtime = 0;
+                }
+            }
+        }
+    }
+}
+
+
+
